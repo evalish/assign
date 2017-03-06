3cb5fa50cc2928788846cf67ef25fc1e13dd782c
evalish <eldarvalishev@gmail.com>
2017-03-07 00:33:21 +0300

    Exercise: Handling Errors in Client-Server Communication using $http

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -7,14 +7,19 @@ angular.module('confusionApp')
     $scope.tab = 1;
     $scope.filtText = '';
     $scope.showDetails = false;
-
-      $scope.dishes= [];
-            menuFactory.getDishes()
-            .then(
-                function(response) {
-                    $scope.dishes = response.data;
-                }
-            );
+    $scope.message = "Loading ...";
+    $scope.dishes = [];
+    menuFactory.getDishes()
+        .then(
+            function(response) {
+                $scope.dishes = response.data;
+                $scope.showMenu = true;
+            },
+            function(response) {
+                $scope.message = "Error: " + response.status + " " +
+                    response.statusText;
+            }
+        );
 
     $scope.select = function(setTab) {
         $scope.tab = setTab;
@@ -87,13 +92,20 @@ angular.module('confusionApp')
         };
     }])
     .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
-        
+
         $scope.dish = {};
-        menuFactory.getDish(parseInt($stateParams.id,10))
-        .then(
-            function(response){
-                $scope.dish = response.data;
-            });
+        $scope.showDish = false;
+        $scope.message = "Loading ...";
+        menuFactory.getDish(parseInt($stateParams.id, 10))
+            .then(
+                function(response) {
+                    $scope.dish = response.data;
+                    $scope.showDish = true;
+                },
+                function(response) {
+                    $scope.message = "Error: " + response.status + " " + response.statusText;
+                }
+            );
     }])
 
 .controller('DishCommentController', ['$scope', function($scope) {
@@ -119,12 +131,20 @@ angular.module('confusionApp')
         };
     }])
     .controller('indexController', ['$scope', 'menuFactory', 'corporateFactory', function($scope, menuFactory, corporateFactory) {
-        
+
         $scope.dish = {}
+        $scope.showDish = false;
+        $scope.message = "Loading ...";
         menuFactory.getDish(0)
-        .then(function(response){
-            $scope.dish = response.data;
-        });
+            .then(
+                function(response) {
+                    $scope.dish = response.data;
+                    $scope.showDish = true;
+                },
+                function(response) {
+                    $scope.message = "Error: " + response.status + " " + response.statusText;
+                }
+            );
         var promotion = menuFactory.getPromotion(0);
         $scope.promotion = promotion;
         var leader = corporateFactory.getLeader(3);
